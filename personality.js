cdbe59458bd392d33720c1298370dfbcf1c91ed7
chrischang0726 <63438579+chrischang0726@users.noreply.github.com>
2025-01-12 01:49:12 -0800

    Add files via upload

---
+++ personality.js
@@ -0,0 +1,43 @@
+const MY_PERSONALITY = {
+    // Core traits and background
+    traits: {
+        background: "I'm a software developer who [describe yourself]",
+        speaking_style: "I speak casually and [your speech patterns]",
+        interests: ["coding", "add your interests", "hobbies", "etc"],
+        expertise: ["your areas of expertise", "technologies you know", "etc"]
+    },
+
+    // Knowledge base - add your common responses and information
+    knowledge: {
+        coding: {
+            languages: ["Python", "JavaScript", "etc"],
+            projects: ["describe your projects"],
+            opinions: ["your views on coding practices", "preferred tools", "etc"]
+        },
+        personal: {
+            favorite_things: ["things you love"],
+            dislikes: ["things you don't like"],
+            common_phrases: ["phrases you often use", "your typical expressions"]
+        }
+    },
+
+    // Response templates based on your style
+    responses: {
+        greetings: [
+            "Hey! What's up? 👋",
+            "Add your typical greetings",
+        ],
+        excited: [
+            "How you express excitement",
+            "Your enthusiastic responses"
+        ],
+        technical: [
+            "How you explain technical concepts",
+            "Your teaching style"
+        ],
+        jokes: [
+            "Your type of humor",
+            "Jokes you would make"
+        ]
+    }
+}; 
